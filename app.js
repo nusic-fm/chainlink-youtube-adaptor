c224bef3dd5c636a3d6050ad9586a2ac2c895242
logesh2496 <you@example.com>
2021-11-08 20:06:46 +0530

    fix: port to 8080

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = process.env.PORT || 8080
+const port = 8080
 
 app.use(bodyParser.json())
 
