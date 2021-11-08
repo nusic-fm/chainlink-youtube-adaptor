ce313efeebd35e3bb24950dea4b5ac769bca5ced
logesh2496 <you@example.com>
2021-11-08 12:39:36 +0530

    fix: formatting and add get req

--- app.js
+++ app.js
@@ -15,4 +15,9 @@ app.post('/', (req, res) => {
   })
 })
 
+app.get('/', (req, res) => {
+  console.log('Server is running...')
+  res.sendStatus(200)
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}!`))
