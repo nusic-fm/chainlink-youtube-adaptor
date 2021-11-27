ad9617e5e41b852a916a044cb3e00e1ae82ad724
logesh2496 <you@example.com>
2021-11-27 11:22:17 +0530

    refactor: app and package

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-console.log('Port: ', process.env.PORT)
+
 const port = process.env.PORT || 8080
 
 app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('API is up and running...')
+})
 
 app.post('/subscribers', (req, res) => {
   console.log('POST Data: ', req.body)
@@ -14,10 +18,6 @@ app.post('/subscribers', (req, res) => {
     console.log('Result: ', result)
     res.status(status).json(result)
   })
-})
-
-app.get('/', (req, res) => {
-  res.send('API is up and running...')
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const { Requester } = require('@chainlink/external-adapter')
 require('dotenv').config()
-console.log({ log: process.env.YOUTUBE_API_KEY })
 
-const YOUTUBE_API_KEY = 'AIzaSyBK86jGrCR8lh5IVyKSohMN1Q2_tvUy2uA'
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY
 // Define custom error scenarios for the API.
 // Return true for the adapter to retry.
 const customError = data => {
@@ -66,34 +65,6 @@ const createRequest = (input, callback) => {
     .catch(error => {
       callback(500, Requester.errored(jobRunID, error))
     })
-}
-
-// This is a wrapper to allow the function to work with
-// GCP Functions
-exports.gcpservice = (req, res) => {
-  createRequest(req.body, (statusCode, data) => {
-    res.status(statusCode).send(data)
-  })
-}
-
-// This is a wrapper to allow the function to work with
-// AWS Lambda
-exports.handler = (event, context, callback) => {
-  createRequest(event, (statusCode, data) => {
-    callback(null, data)
-  })
-}
-
-// This is a wrapper to allow the function to work with
-// newer AWS Lambda implementations
-exports.handlerv2 = (event, context, callback) => {
-  createRequest(JSON.parse(event.body), (statusCode, data) => {
-    callback(null, {
-      statusCode: statusCode,
-      body: JSON.stringify(data),
-      isBase64Encoded: false
-    })
-  })
 }
 
 // This allows the function to be exported for testing
